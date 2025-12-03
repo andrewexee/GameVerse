e566c42eb198532325d17ae4c3dfa9b315a6782d
ElGolondras <jfoncla2@institutodh.net>
2025-12-03 17:17:00 +0100

    Prueba 2

--- app/src/main/java/com/paworo06/gameverse/MainActivity.kt
+++ app/src/main/java/com/paworo06/gameverse/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Hola Prueba")
+            Text("Hola")
         }
     }
 }
